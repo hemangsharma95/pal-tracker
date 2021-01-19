67c73dd09110108c50e3d5e0dcd289cdcba60896
Ubuntu <ubuntu@ip-172-31-90-125.ec2.internal>
2021-01-19 16:54:17 +0000

    clean up

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,2 +1,19 @@
-package io.pivotal.pal.tracker;public class WelcomeController {
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    String greeting;
+
+    public WelcomeController(@Value("${welcome.message}") String message) {
+        this.greeting = message;
+    }
+
+    @GetMapping("/")
+    public String sayHello() {
+        return this.greeting;
+    }
 }
